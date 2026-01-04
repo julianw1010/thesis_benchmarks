943e81e42878ad68a731527dbbea1e40683b7295
Julian10 <Julian10@node0.pagetables.pagetable-pg0.clemson.cloudlab.us>
2026-01-04 20:53:30 +0000

    Updated page number

--- scripts/run_f9_one.sh
+++ scripts/run_f9_one.sh
@@ -18,7 +18,7 @@
 
 #PERF_EVENTS=cycles,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,dtlb_load_misses.walk_duration,dtlb_store_misses.walk_duration
 PERF_EVENTS=cycles,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,dtlb_load_misses.walk_duration,dtlb_store_misses.walk_duration,page_walker_loads.dtlb_l1,page_walker_loads.dtlb_l2,page_walker_loads.dtlb_l3,page_walker_loads.dtlb_memory,page_walker_loads.dtlb_l1,page_walker_loads.dtlb_l2,page_walker_loads.dtlb_l3,page_walker_loads.dtlb_memory,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses
-NR_PTCACHE_PAGES=1100000 # --- 2GB per socket
+NR_PTCACHE_PAGES=1000000 # --- 2GB per socket
 XSBENCH_ARGS=" -- -p 25000000 -g 300000 "
 GRAPH500_ARGS=" -- -s 29 -e 21"
 HASHJOIN_ARGS=" -- -o 1250000000 -i 10000000 -s 10000000"
