dd5b8353f3f671941fd397d3b7b53498ae93f78f
Julian10 <Julian10@node0.julian10-286102.pagetable-pg0.clemson.cloudlab.us>
2026-01-12 15:52:36 +0000

    Removed package

--- sources/prepare.sh
+++ sources/prepare.sh
@@ -2,7 +2,7 @@ sudo apt-get install build-essential libncurses-dev \
                      bison flex libssl-dev libelf-dev \
                      libnuma-dev python3 python3 python3-pip \
                      python3-matplotlib python3-numpy \
-                     git wget kernel-package fakeroot ccache \
+                     git wget fakeroot ccache \
                      libncurses5-dev wget pandoc libevent-dev \
                      libreadline-dev python3-setuptools
 
