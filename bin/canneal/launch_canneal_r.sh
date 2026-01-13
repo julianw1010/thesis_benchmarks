16cbdb88744e9557fc0d5c099373bdb18852d0f8
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-13 02:01:45 +0000

    Updated scripts

--- bin/canneal/launch_canneal_r.sh
+++ bin/canneal/launch_canneal_r.sh
@@ -1,1 +1,1 @@
-numactl -r all /usr/bin/time --verbose -- ./bench_canneal_mt 64 50000 2000 ../datasets/canneal_35gb_int 400
+numactl -r all /usr/bin/time --verbose -- ./bench_canneal_mt 64 50000 2000 ../../datasets/canneal_35gb_int 400
