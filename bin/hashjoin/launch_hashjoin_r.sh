e29a8201d49623dd3fd724b780779f8b65fb3a81
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-13 13:52:28 +0000

    Updated launch scritps

--- bin/hashjoin/launch_hashjoin_r.sh
+++ bin/hashjoin/launch_hashjoin_r.sh
@@ -1,1 +1,1 @@
-numactl -r all /usr/bin/time --verbose -- ./bench_hashjoin_mt -- -o 1675000000 -i 16750000 -s 16750000
+numactl -r all /usr/bin/time --verbose -- ./bench_hashjoin_mt -- -o 1675000000 -i 16750000 -s 16750000 -n 25
