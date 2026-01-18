0ca17feea93345c79cddca809ccf64a146ac4b4c
Julian10 <Julian10@node1.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-18 17:15:51 +0000

    Added scripts

---
+++ bin/xsbench/hydra/launch_xsbench_r_i.sh
@@ -0,0 +1,3 @@
+echo -1 | sudo tee /proc/hydra/history
+numactl -r all -i all /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000
+cat /proc/hydra/history > history_r_i.txt
---
+++ bin/xsbench/linux/launch_xsbench_i.sh
@@ -0,0 +1,3 @@
+echo -1 | sudo tee /proc/mitosis/history
+numactl -P -i all /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000
+cat /proc/mitosis/history > history_i.txt
