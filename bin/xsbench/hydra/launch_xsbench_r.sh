f23a1c521c6006eecc78d9f0639eb80a3a2fad19
Julian10 <Julian10@node3.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-18 17:21:38 +0000

    Updated hydra scripts

--- bin/xsbench/hydra/launch_xsbench_r.sh
+++ bin/xsbench/hydra/launch_xsbench_r.sh
@@ -1,3 +1,20 @@
-echo -1 | sudo tee /proc/hydra/history
-numactl -r all /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000
-cat /proc/hydra/history > history_r.txt
+#!/bin/bash
+
+# Set tlbflush opt once
+echo 1 | sudo tee /proc/hydra/tlbflush_opt
+
+for i in {0..9}; do
+    echo "=== Running with repl_order=$i ==="
+    
+    # Reset history
+    echo -1 | sudo tee /proc/hydra/history
+    
+    # Set replication order
+    echo $i | sudo tee /proc/hydra/repl_order
+    
+    # Run benchmark
+    numactl -r all /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000
+    
+    # Save history with suffix
+    cat /proc/hydra/history > history_${i}.txt
+done
