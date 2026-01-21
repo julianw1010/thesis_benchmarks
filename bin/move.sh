02daa1e0ee07e0f7998dd3ed26c42589bd026b16
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-21 11:02:10 +0000

    Updated scripts

--- bin/move.sh
+++ bin/move.sh
@@ -1,9 +1,23 @@
-mv bench_canneal_* canneal
-mv bench_btree_* btree
-mv bench_hashjoin_* hashjoin
-mv bench_gups_* gups
-mv bench_redis_* redis
-mv bench_xsbench_* xsbench
-mv bench_pr_* pr
-mv bench_stream stream
-mv bench_liblinear_* liblinear
+#!/bin/bash
+
+# Create subfolders if they don't exist
+mkdir -p canneal btree hashjoin gups redis xsbench pr stream liblinear
+mkdir -p bc bfs cc cc_sv pr_spmv
+
+# Move benchmarks to their respective subfolders
+mv bench_canneal_* canneal/ 2>/dev/null
+mv bench_btree_* btree/ 2>/dev/null
+mv bench_hashjoin_* hashjoin/ 2>/dev/null
+mv bench_gups_* gups/ 2>/dev/null
+mv bench_redis_* redis/ 2>/dev/null
+mv bench_xsbench_* xsbench/ 2>/dev/null
+mv bench_liblinear_* liblinear/ 2>/dev/null
+mv bench_stream stream/ 2>/dev/null
+
+# GAP benchmarks
+mv bench_pr_* pr/ 2>/dev/null
+mv bench_bc_* bc/ 2>/dev/null
+mv bench_bfs_* bfs/ 2>/dev/null
+mv bench_cc_sv_* cc_sv/ 2>/dev/null
+mv bench_cc_* cc/ 2>/dev/null
+mv bench_pr_spmv_* pr_spmv/ 2>/dev/null
