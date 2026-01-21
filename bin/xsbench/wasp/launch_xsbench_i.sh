f491af5d84b0150f1f63477dc8e75d960adbc442
Julian10 <Julian10@node2.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-21 08:00:52 +0000

    Updated script

--- bin/xsbench/wasp/launch_xsbench_i.sh
+++ bin/xsbench/wasp/launch_xsbench_i.sh
@@ -5,7 +5,7 @@ echo 500000 | sudo tee /proc/mitosis/cache
 # Find the starting point based on existing history files
 start=0
 for i in {2..0}; do
-    if [[ -f "history_${i}.txt" ]]; then
+    if [[ -f "history_i_${i}.txt" ]]; then
         start=$((i + 1))
         break
     fi
@@ -30,9 +30,9 @@ for i in $(seq $start 2); do
     echo -1 | sudo tee /proc/mitosis/history
     
     # Run benchmark
-    script -e -q -c "numactl -P -i all /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000" output_${i}.txt
+    script -e -q -c "numactl -P -i all /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000" output_i_${i}.txt
     [[ $? -eq 130 ]] && { echo "Interrupted. Exiting..."; exit 1; }
     
     # Save history with suffix
-    cat /proc/mitosis/history > history_${i}.txt
+    cat /proc/mitosis/history > history_i_${i}.txt
 done
