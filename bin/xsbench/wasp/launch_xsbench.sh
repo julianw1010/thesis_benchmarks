d2be6e39eebca02b8c4b1f7aa62013453c0a28f1
Julian10 <Julian10@node2.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-21 01:18:19 +0000

    Updated scripts

--- bin/xsbench/wasp/launch_xsbench.sh
+++ bin/xsbench/wasp/launch_xsbench.sh
@@ -4,21 +4,21 @@ echo 500000 | sudo tee /proc/mitosis/cache
 
 # Find the starting point based on existing history files
 start=0
-for i in {2..0}; do
+for i in {4..0}; do
     if [[ -f "history_${i}.txt" ]]; then
         start=$((i + 1))
         break
     fi
 done
 
-if [[ $start -gt 2 ]]; then
-    echo "All runs (0-2) already completed."
+if [[ $start -gt 4 ]]; then
+    echo "All runs (0-4) already completed."
     exit 0
 fi
 
 echo "Continuing from i=$start"
 
-for i in $(seq $start 2); do
+for i in $(seq $start 4); do
     [[ $? -eq 130 ]] && { echo "Interrupted. Exiting..."; exit 1; }
     echo "=== Running iteration=$i ==="
     
@@ -30,7 +30,7 @@ for i in $(seq $start 2); do
     echo -1 | sudo tee /proc/mitosis/history
     
     # Run benchmark
-    script -e -q -c "numactl -P /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000" output_${i}.txt
+    script -e -q -c "numactl -P -O all /usr/bin/time --verbose -- ../bench_xsbench_mt -- -p 25000000 -g 400000" output_${i}.txt
     [[ $? -eq 130 ]] && { echo "Interrupted. Exiting..."; exit 1; }
     
     # Save history with suffix
