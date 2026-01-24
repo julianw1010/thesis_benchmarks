5ad0cc17928a79c1a23d19bcb63c1c15bca3bd56
Julian10 <Julian10@node1.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-24 14:35:31 +0000

    Updated scritps

--- bin/memcached/launch_benchmark.sh
+++ bin/memcached/launch_benchmark.sh
@@ -107,7 +107,7 @@ for ((i=start; i<=max_index; i++)); do
     echo "Populating memcached..."
     ./bench_memtier \
         -s localhost -p 11211 --protocol=memcache_text \
-        --key-minimum=1 --key-maximum=1730000000 --key-pattern=P:P \
+        --key-minimum=1 --key-maximum=500000000 --key-pattern=P:P \
         --ratio=1:0 --data-size=24 --threads=128 --clients=20 \
         --pipeline=100 -n 680000 --hide-histogram
 
@@ -117,7 +117,7 @@ for ((i=start; i<=max_index; i++)); do
     echo "Running benchmark..."
     script -e -q -c "/usr/bin/time --verbose -- ./bench_memtier \
         -s localhost -p 11211 --protocol=memcache_text \
-        --key-minimum=1 --key-maximum=1730000000 --key-pattern=R:R \
+        --key-minimum=1 --key-maximum=500000000 --key-pattern=R:R \
         --ratio=0:1 --data-size=24 --threads=32 --clients=20 \
         --pipeline=100 --test-time=300" "${output_folder}/output_${prefix}${i}.txt"
 
--- bin/memcached/linux.sh
+++ bin/memcached/linux.sh
@@ -1,12 +1,2 @@
-sudo pkill memcached
-numactl -P memcached -m 220000 -t 32 -p 11211 -c 8192 -d
-memtier_benchmark \
-    -s localhost -p 11211 --protocol=memcache_text \
-    --key-minimum=1 --key-maximum=1730000000 --key-pattern=P:P \
-    --ratio=1:0 --data-size=24 --threads=128 --clients=20 \
-    --pipeline=100 -n 680000 --hide-histogram
-memtier_benchmark \
-    -s localhost -p 11211 --protocol=memcache_text \
-    --key-minimum=1 --key-maximum=1730000000 --key-pattern=R:R \
-    --ratio=0:1 --data-size=24 --threads=32 --clients=20 \
-    --pipeline=100 --test-time=300
+./launch_benchmark.sh 0 3 linux
+./launch_benchmark.sh 1 3 linux
