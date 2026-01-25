c43784cadaacb5a893ec3c6ffa2143ef68a49870
Julian10 <Julian10@node1.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-25 17:15:14 +0000

    Modified scripts

--- bin/pr/linux.sh
+++ bin/pr/linux.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 1 linux ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 3
-../launch_benchmark.sh 1 1 linux ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 3
+../launch_benchmark.sh 0 1 linux/kron29 ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 10
+../launch_benchmark.sh 1 1 linux/kron29 ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 10
+../launch_benchmark.sh 0 1 linux/uni29 ./bench_pr_mt -f ../../datasets/graphs/uni29.sg -n 10
+../launch_benchmark.sh 1 1 linux/uni29 ./bench_pr_mt -f ../../datasets/graphs/uni29.sg -n 10
+../launch_benchmark.sh 0 1 linux/web ./bench_pr_mt -f ../../datasets/graphs/web.sg -n 10
+../launch_benchmark.sh 1 1 linux/web ./bench_pr_mt -f ../../datasets/graphs/web.sg -n 10
+../launch_benchmark.sh 0 1 linux/twitter ./bench_pr_mt -f ../../datasets/graphs/twitter.sg -n 10
+../launch_benchmark.sh 1 1 linux/twitter ./bench_pr_mt -f ../../datasets/graphs/twitter.sg -n 10
+../launch_benchmark.sh 0 1 linux/road ./bench_pr_mt -f ../../datasets/graphs/road.sg -n 10
+../launch_benchmark.sh 1 1 linux/road ./bench_pr_mt -f ../../datasets/graphs/road.sg -n 10
--- bin/pr/mitosis.sh
+++ bin/pr/mitosis.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
-
-../launch_benchmark.sh 2 1 mitosis ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 3
-../launch_benchmark.sh 3 1 mitosis ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 3
+../launch_benchmark.sh 2 1 mitosis/kron29 ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 10
+../launch_benchmark.sh 3 1 mitosis/kron29 ./bench_pr_mt -f ../../datasets/graphs/kron29.sg -n 10
+../launch_benchmark.sh 2 1 mitosis/uni29 ./bench_pr_mt -f ../../datasets/graphs/uni29.sg -n 10
+../launch_benchmark.sh 3 1 mitosis/uni29 ./bench_pr_mt -f ../../datasets/graphs/uni29.sg -n 10
+../launch_benchmark.sh 2 1 mitosis/web ./bench_pr_mt -f ../../datasets/graphs/web.sg -n 10
+../launch_benchmark.sh 3 1 mitosis/web ./bench_pr_mt -f ../../datasets/graphs/web.sg -n 10
+../launch_benchmark.sh 2 1 mitosis/twitter ./bench_pr_mt -f ../../datasets/graphs/twitter.sg -n 10
+../launch_benchmark.sh 3 1 mitosis/twitter ./bench_pr_mt -f ../../datasets/graphs/twitter.sg -n 10
+../launch_benchmark.sh 2 1 mitosis/road ./bench_pr_mt -f ../../datasets/graphs/road.sg -n 10
+../launch_benchmark.sh 3 1 mitosis/road ./bench_pr_mt -f ../../datasets/graphs/road.sg -n 10
