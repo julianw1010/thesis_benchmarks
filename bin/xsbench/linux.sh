61a69989cd23478d2c9e3fc5802c4cc2a9577c2b
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-26 15:32:15 +0000

    Updated scripts

--- bin/xsbench/linux.sh
+++ bin/xsbench/linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 3 linux/50gb ./bench_xsbench_mt -- -p 25000000 -g 100000
+../launch_benchmark.sh 0 3 linux/50gb ./bench_xsbench_mt -- -p 250000 -g 10000
 ../launch_benchmark.sh 1 3 linux/50gb ./bench_xsbench_mt -- -p 25000000 -g 100000
 ../launch_benchmark.sh 0 3 linux/100gb ./bench_xsbench_mt -- -p 25000000 -g 200000
 ../launch_benchmark.sh 1 3 linux/100gb ./bench_xsbench_mt -- -p 25000000 -g 200000
