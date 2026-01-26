2d711d366f72ac6d99cd14bd04439562c240c648
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-26 13:34:35 +0000

    Moved files

--- bin/xsbench/linux.sh
+++ bin/xsbench/linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 3 linux ./bench_xsbench_mt -- -p 25000000 -g 100000
-../launch_benchmark.sh 1 3 linux ./bench_xsbench_mt -- -p 25000000 -g 100000
+../launch_benchmark.sh 0 3 linux/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 1 3 linux/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
