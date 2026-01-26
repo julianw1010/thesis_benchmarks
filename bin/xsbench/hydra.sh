3227ac5aab23b27b2b3c6244dc28120a5f305ff3
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-26 13:41:52 +0000

    Updated scripts

--- bin/xsbench/hydra.sh
+++ bin/xsbench/hydra.sh
@@ -7,7 +7,11 @@ echo 1 | sudo tee /proc/hydra/tlbflush_opt
 for repl_order in {0..9}; do
     echo "=== Setting repl_order=$repl_order ==="
     echo $repl_order | sudo tee /proc/hydra/repl_order
-    
-    ../launch_benchmark.sh 2 3 "hydra/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 100000
-    ../launch_benchmark.sh 3 3 "hydra/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 100000
+
+    ../launch_benchmark.sh 2 3 "hydra/50gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 100000
+    ../launch_benchmark.sh 3 3 "hydra/50gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 100000
+    ../launch_benchmark.sh 2 3 "hydra/100gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 200000
+    ../launch_benchmark.sh 3 3 "hydra/100gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 200000    
+    ../launch_benchmark.sh 2 3 "hydra/200gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 400000
+    ../launch_benchmark.sh 3 3 "hydra/200gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 400000
 done
--- bin/xsbench/mitosis.sh
+++ bin/xsbench/mitosis.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 3 mitosis ./bench_xsbench_mt -- -p 25000000 -g 100000
-../launch_benchmark.sh 3 3 mitosis ./bench_xsbench_mt -- -p 25000000 -g 100000
+../launch_benchmark.sh 2 3 mitosis/50gb ./bench_xsbench_mt -- -p 25000000 -g 100000
+../launch_benchmark.sh 3 3 mitosis/50gb ./bench_xsbench_mt -- -p 25000000 -g 100000
+../launch_benchmark.sh 2 3 mitosis/100gb ./bench_xsbench_mt -- -p 25000000 -g 200000
+../launch_benchmark.sh 3 3 mitosis/100gb ./bench_xsbench_mt -- -p 25000000 -g 200000
+../launch_benchmark.sh 2 3 mitosis/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 3 3 mitosis/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
