21572074929167564c9eb8f1179c702bde317c0c
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-26 09:48:44 +0000

    Updated makefile

--- bin/move.sh
+++ bin/move.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# Directories to process
-dirs=("bc" "pr" "cc" "cc_sv" "pr_spmv" "bfs")
+# Create subfolders if they don't exist
+mkdir -p canneal btree hashjoin gups redis xsbench pr stream liblinear memcached
+mkdir -p bc bfs cc cc_sv pr_spmv
 
-# Subfolders to move
-subfolders=("linux" "hydra" "wasp" "mitosis")
+# Move benchmarks to their respective subfolders
+mv bench_canneal_* canneal/ 2>/dev/null
+mv bench_btree_* btree/ 2>/dev/null
+mv bench_hashjoin_* hashjoin/ 2>/dev/null
+mv bench_gups_* gups/ 2>/dev/null
+mv bench_redis_* redis/ 2>/dev/null
+mv bench_xsbench_* xsbench/ 2>/dev/null
+mv bench_liblinear_* liblinear/ 2>/dev/null
+mv bench_stream stream/ 2>/dev/null
+mv bench_memcached memcached/ 2>/dev/null
+mv bench_memtier memcached/ 2>/dev/null
 
-for dir in "${dirs[@]}"; do
-    mkdir -p "$dir/oldresults"
-    
-    for sub in "${subfolders[@]}"; do
-        if [ -d "$dir/$sub" ]; then
-            mv "$dir/$sub" "$dir/oldresults/"
-            echo "Moved $dir/$sub to $dir/oldresults/"
-        fi
-    done
-done
-
-echo "Done!"
+# GAP benchmarks (order matters: longer prefixes first)
+mv bench_pr_spmv_* pr_spmv/ 2>/dev/null
+mv bench_pr_* pr/ 2>/dev/null
+mv bench_bc_* bc/ 2>/dev/null
+mv bench_bfs_* bfs/ 2>/dev/null
+mv bench_cc_sv_* cc_sv/ 2>/dev/null
+mv bench_cc_* cc/ 2>/dev/null
