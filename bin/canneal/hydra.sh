cdf5b56c5bcd83e63f14e262f905e53b1c2d5382
Julian10 <Julian10@node1.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-27 18:34:28 +0000

    Updated scripts

--- bin/canneal/hydra.sh
+++ bin/canneal/hydra.sh
@@ -8,6 +8,6 @@ for repl_order in {0..9}; do
     echo "=== Setting repl_order=$repl_order ==="
     echo $repl_order | sudo tee /proc/hydra/repl_order
     
-    ../launch_benchmark.sh 2 3 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
-    ../launch_benchmark.sh 3 3 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
+    ../launch_benchmark.sh 2 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
+    ../launch_benchmark.sh 3 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
 done
--- bin/canneal/linux.sh
+++ bin/canneal/linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 3 linux ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
-../launch_benchmark.sh 1 3 linux ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
+../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
+../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
