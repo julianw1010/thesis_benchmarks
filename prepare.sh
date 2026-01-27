f565d8d646157b83b14c1a662da261a727636428
Julian Waciewski <Julian10@node0.pagetables-intel.pagetable.emulab.net>
2026-01-27 12:19:03 -0700

    Updated scripts

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build-essential libncurses-dev \
+sudo apt-get -y install build-essential libncurses-dev \
                      bison flex libssl-dev libelf-dev \
                      libnuma-dev python3 python3 python3-pip \
                      python3-matplotlib python3-numpy \
