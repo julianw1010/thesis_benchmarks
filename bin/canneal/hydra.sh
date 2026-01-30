2f67a057d8c0cb8add16d9ddc311b3cd156fc7c3
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-30 15:35:04 +0000

    Updated scripts

--- bin/canneal/hydra.sh
+++ bin/canneal/hydra.sh
@@ -4,10 +4,10 @@ set -e
 
 echo 1 | sudo tee /proc/hydra/tlbflush_opt
 
-for repl_order in {0..9}; do
+for repl_order in 9; do
     echo "=== Setting repl_order=$repl_order ==="
     echo $repl_order | sudo tee /proc/hydra/repl_order
     
-    ../launch_benchmark.sh 2 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
-    ../launch_benchmark.sh 3 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_35gb_int 400
+    ../launch_benchmark.sh 2 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_40gb_int 400
+    ../launch_benchmark.sh 3 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_40gb_int 400
 done
