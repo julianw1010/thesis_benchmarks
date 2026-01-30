79cbd6f44df94af7298266c27cd9789e070b2068
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-30 09:11:08 +0000

    Updated scripts

--- bin/xsbench/mitosis.sh
+++ bin/xsbench/mitosis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 5 mitosis/200gb ./bench_xsbench_mt -- -p 7500000 -g 200000
-../launch_benchmark.sh 3 5 mitosis/200gb ./bench_xsbench_mt -- -p 7500000 -g 200000
+../launch_benchmark.sh 2 5 mitosis/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 3 5 mitosis/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
--- bin/xsbench/wasp.sh
+++ bin/xsbench/wasp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 wasp/200gb ./bench_xsbench_mt -- -p 7500000 -g 200000
-../launch_benchmark.sh 1 5 wasp/200gb ./bench_xsbench_mt -- -p 7500000 -g 200000
+../launch_benchmark.sh 0 5 wasp/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 1 5 wasp/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
