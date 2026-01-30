f14d2ebdb3827d3715343e54457977ac33905f68
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-30 19:34:09 +0000

    Updated scritps

--- bin/canneal/linux.sh
+++ bin/canneal/linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 128 200000 2000 ../../datasets/canneal_40gb_int 400
-../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 128 200000 2000 ../../datasets/canneal_40gb_int 400
+../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 128 1200000 2000 ../../datasets/canneal_40gb_int 2400
+../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 128 1200000 2000 ../../datasets/canneal_40gb_int 2400
