f2422ecad4f6b1bdafb67b0466f7a3c47f3a8dbb
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-30 19:16:16 +0000

    Updated scripts

--- bin/canneal/mitosis.sh
+++ bin/canneal/mitosis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 5 mitosis ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_40gb_int 400
-../launch_benchmark.sh 3 5 mitosis ./bench_canneal_mt 64 200000 2000 ../../datasets/canneal_40gb_int 400
+../launch_benchmark.sh 2 5 mitosis ./bench_canneal_mt 128 200000 2000 ../../datasets/canneal_40gb_int 400
+../launch_benchmark.sh 3 5 mitosis ./bench_canneal_mt 128 200000 2000 ../../datasets/canneal_40gb_int 400
