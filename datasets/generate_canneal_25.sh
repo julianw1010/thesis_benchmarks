559441b1f9395ce119781a56dd3a9182c88df790
Julian Waciewski <Julian10@node0.pagetables-intel.pagetable.emulab.net>
2026-01-30 22:26:35 -0700

    Removed scripts

--- datasets/generate_canneal_25.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -f canneal_25gb_int ]; then
-    echo "canneal_25gb_int already exists. Skipping generation."
-    exit 0
-fi
-[ ! -f fast_canneal_int ] && gcc -O3 -fopenmp -o fast_canneal_int fast_canneal_int.c
-./fast_canneal_int 20000 20000 397000000 > canneal_25gb_int
