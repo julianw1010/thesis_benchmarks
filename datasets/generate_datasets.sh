35ae98e3d0dad25cdeefeebccbb47b00fb642cbb
Julian Waciewski <Julian10@node1.pagetables-intel.pagetable.emulab.net>
2026-01-30 21:46:43 -0700

    Updated scritps

--- datasets/generate_datasets.sh
+++ datasets/generate_datasets.sh
@@ -5,6 +5,7 @@ mkdir -p graphs
 ./generate_canneal_large.sh
 ./generate_canneal_big.sh
 ./generate_canneal_huge.sh
+./generate_canneal_30gb.sh
 ./generate_liblinear_large.sh
 ./generate_liblinear_small.sh
 ./generate_graphs.sh
