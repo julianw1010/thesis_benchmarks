4b267fd71fa199483d849ed55d812275c2a08052
Julian Waciewski <Julian10@node0.pagetables-intel.pagetable.emulab.net>
2026-01-31 11:13:26 -0700

    Updated scripts

--- bin/canneal/linux_intel.sh
+++ bin/canneal/linux_intel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 64 500000 2000 ../../datasets/canneal_25gb_int 2400
-../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 64 500000 2000 ../../datasets/canneal_25gb_int 2400
+../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 64 500000 2000 ../../datasets/canneal_20gb_int 2400
+../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 64 500000 2000 ../../datasets/canneal_20gb_int 2400
