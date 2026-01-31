975cc5b654727b5ad560b03adaa343e11dfc54ad
Julian Waciewski <Julian10@node0.pagetables-intel.pagetable.emulab.net>
2026-01-30 22:06:05 -0700

    Added generator

--- bin/canneal/linux.sh
+++ bin/canneal/linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 128 2400000 2000 ../../datasets/canneal_40gb_int 2400
-../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 128 2400000 2000 ../../datasets/canneal_40gb_int 2400
+../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 128 240000 2000 ../../datasets/canneal_30gb_int 240
+../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 128 240000 2000 ../../datasets/canneal_30gb_int 240
