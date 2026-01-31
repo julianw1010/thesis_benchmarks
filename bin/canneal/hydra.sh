3f50620def3d19d0d503e51a5f5589d24f90d51c
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-31 03:17:00 +0000

    Updated parameters

--- bin/canneal/hydra.sh
+++ bin/canneal/hydra.sh
@@ -8,6 +8,6 @@ for repl_order in 9; do
     echo "=== Setting repl_order=$repl_order ==="
     echo $repl_order | sudo tee /proc/hydra/repl_order
     
-    ../launch_benchmark.sh 2 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 128 1200000 2000 ../../datasets/canneal_40gb_int 2400
-    ../launch_benchmark.sh 3 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 128 1200000 2000 ../../datasets/canneal_40gb_int 2400
+    ../launch_benchmark.sh 2 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 128 2400000 2000 ../../datasets/canneal_40gb_int 2400
+    ../launch_benchmark.sh 3 5 "hydra/repl_order_${repl_order}" ./bench_canneal_mt 128 2400000 2000 ../../datasets/canneal_40gb_int 2400
 done
