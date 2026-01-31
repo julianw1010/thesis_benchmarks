4005169f8f7f2cff0740a4b86fdbf29f54cf9de8
Julian10 <Julian10@node0.pagetables-2.pagetable-pg0.clemson.cloudlab.us>
2026-01-31 20:19:09 +0000

    Updated scripts

--- bin/xsbench/mitosis_intel.sh
+++ bin/xsbench/mitosis_intel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 5 intel/mitosis/100gb ./bench_xsbench_mt -- -p 7500000 -g 200000
-../launch_benchmark.sh 3 5 intel/mitosis/100gb ./bench_xsbench_mt -- -p 7500000 -g 200000
+../launch_benchmark.sh 2 5 intel/mitosis/100gb ./bench_xsbench_mt -- -p 25000000 -g 600000
+../launch_benchmark.sh 3 5 intel/mitosis/100gb ./bench_xsbench_mt -- -p 25000000 -g 600000
