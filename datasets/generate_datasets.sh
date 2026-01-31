2f3ad080faa7f88155f3f106ef9a2139099583fd
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-31 04:19:37 +0000

    Updated scripts

--- datasets/generate_datasets.sh
+++ datasets/generate_datasets.sh
@@ -4,6 +4,7 @@ mkdir -p graphs
 ./generate_canneal_medium.sh
 ./generate_canneal_large.sh
 ./generate_canneal_big.sh
+./generate_canneal_huge.sh
 ./generate_liblinear_large.sh
 ./generate_liblinear_small.sh
 ./generate_graphs.sh
