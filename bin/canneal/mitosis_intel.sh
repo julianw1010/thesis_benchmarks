95638f3937de7259dbf734bf512940d7b71b8582
Julian Waciewski <Julian10@node0.pagetables-intel.pagetable.emulab.net>
2026-01-30 22:41:40 -0700

    Updated scripts

--- bin/canneal/mitosis_intel.sh
+++ bin/canneal/mitosis_intel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 5 mitosis ./bench_canneal_mt 128 500000 2000 ../../datasets/canneal_25gb_int 2400
-../launch_benchmark.sh 3 5 mitosis ./bench_canneal_mt 128 500000 2000 ../../datasets/canneal_25gb_int 2400
+../launch_benchmark.sh 2 5 mitosis ./bench_canneal_mt 64 500000 2000 ../../datasets/canneal_25gb_int 2400
+../launch_benchmark.sh 3 5 mitosis ./bench_canneal_mt 64 500000 2000 ../../datasets/canneal_25gb_int 2400
