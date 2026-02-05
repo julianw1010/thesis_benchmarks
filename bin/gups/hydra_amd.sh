bcca3f65b1b69985df78fc171b1af407a823e742
Julian10 <Julian10@node0.pagetables-4-amd.pagetable-pg0.clemson.cloudlab.us>
2026-02-05 15:06:48 +0000

    Updated scripts

--- bin/gups/hydra_amd.sh
+++ bin/gups/hydra_amd.sh
@@ -8,6 +8,6 @@ for repl_order in 9; do
     echo "=== Setting repl_order=$repl_order ==="
     echo $repl_order | sudo tee /proc/hydra/repl_order
 
-    ../launch_benchmark.sh 2 5 "amd/hydra/200gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 400000
-    ../launch_benchmark.sh 3 5 "amd/hydra/200gb/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 400000
+    ../launch_benchmark.sh 2 5 "amd/hydra/repl_order_${repl_order}/" ./bench_gups_mt -- 128
+    ../launch_benchmark.sh 3 5 "amd/hydra/repl_order_${repl_order}/" ./bench_gups_mt -- 128
 done
--- bin/gups/linux_amd.sh
+++ bin/gups/linux_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 amd/linux/200gb ./bench_gups_mt -- 128
-../launch_benchmark.sh 1 5 amd/linux/200gb ./bench_gups_mt -- 128
+../launch_benchmark.sh 0 5 amd/linux/ ./bench_gups_mt -- 128
+../launch_benchmark.sh 1 5 amd/linux/ ./bench_gups_mt -- 128
