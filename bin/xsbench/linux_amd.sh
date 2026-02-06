58c77890a0bd38271c0c75b6331bd1e1442176e2
Julian10 <Julian10@node0.pagetables-4-amd.pagetable-pg0.clemson.cloudlab.us>
2026-02-06 13:25:19 +0000

    Updated scripts

--- bin/xsbench/linux_amd.sh
+++ bin/xsbench/linux_amd.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -e
+#!/bin/bashset -e
 
-../launch_benchmark.sh 0 5 amd/linux/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
-../launch_benchmark.sh 1 5 amd/linux/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 0 5 amd/linux/ ./bench_xsbench_mt -- -p 25000000 -g 400000 -t 32
+../launch_benchmark.sh 1 5 amd/linux/ ./bench_xsbench_mt -- -p 25000000 -g 400000 -t 32
