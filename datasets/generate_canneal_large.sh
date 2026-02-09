232c5945b49799e6218c79fc9fbb53608337238d
Julian W <j.w10@freenet.de>
2026-02-09 10:29:31 +0000

    Updated scripts

--- datasets/generate_canneal_large.sh
+++ datasets/generate_canneal_large.sh
@@ -3,4 +3,4 @@ if [ -f canneal_35gb_int ]; then
     exit 0
 fi
 [ ! -f fast_canneal_int ] && gcc -O3 -fopenmp -o fast_canneal_int fast_canneal_int.c
-./fast_canneal_int 24000 24000 555000000 > canneal_35gb_int
+./fast_canneal_int 24000 24000 555000000 > canneal_large
--- datasets/generate_canneal_small.sh
+++ datasets/generate_canneal_small.sh
@@ -3,4 +3,4 @@ if [ -f canneal_3gb_int ]; then
     exit 0
 fi
 [ ! -f fast_canneal_int ] && gcc -O3 -fopenmp -o fast_canneal_int fast_canneal_int.c
-./fast_canneal_int 7800 7800 57700000 > canneal_3gb_int
+./fast_canneal_int 7800 7800 57700000 > canneal_small
