f8cb145e031895a72f1c8b0e0571e045a3c968ff
Julian W <j.w10@freenet.de>
2026-02-13 16:09:25 +0000

    Updated benchmarks

--- bin/xsbench/mitosis_amd.sh
+++ bin/xsbench/mitosis_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 5 amd/mitosis/ ./bench_xsbench_mt -- -p 25000000 -g 600000
-../launch_benchmark.sh 3 5 amd/mitosis/ ./bench_xsbench_mt -- -p 25000000 -g 600000
+../launch_benchmark.sh 2 5 mitosis ./bench_xsbench_mt -- -p 2500000 -g 200000
+../launch_benchmark.sh 3 5 mitosis ./bench_xsbench_mt -- -p 2500000 -g 200000
