9f28a99fce9a495a855de4b571c20e80d7e568bc
Julian W <j.w10@freenet.de>
2026-02-13 17:28:04 +0000

    Added results

--- bin/xsbench/wasp_amd.sh
+++ bin/xsbench/wasp_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 wasp ./bench_xsbench_mt -- -p 2500000 -g 200000
-../launch_benchmark.sh 1 5 wasp ./bench_xsbench_mt -- -p 2500000 -g 200000
+../launch_benchmark.sh 0 5 wasp ./bench_xsbench_mt -- -p 25000000 -g 600000
+../launch_benchmark.sh 1 5 wasp ./bench_xsbench_mt -- -p 25000000 -g 600000
