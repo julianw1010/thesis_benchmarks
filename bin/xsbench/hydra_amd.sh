0c6e53ec85f6300947b9e35deb71eda4a58cf9c5
Julian W <j.w10@freenet.de>
2026-02-18 12:17:04 +0000

    Deleted files

--- bin/xsbench/hydra_amd.sh
+++ bin/xsbench/hydra_amd.sh
@@ -8,6 +8,6 @@ for repl_order in 9; do
     echo "=== Setting repl_order=$repl_order ==="
     echo $repl_order | sudo tee /proc/hydra/repl_order
 
-    ../launch_benchmark.sh 2 5 "hydra/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 600000
-    ../launch_benchmark.sh 3 5 "hydra/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 25000000 -g 600000
+    ../launch_benchmark.sh 2 1 "hydra/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 2500000 -g 200000
+    ../launch_benchmark.sh 3 1 "hydra/repl_order_${repl_order}" ./bench_xsbench_mt -- -p 2500000 -g 200000
 done
