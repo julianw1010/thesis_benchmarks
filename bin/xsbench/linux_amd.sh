72d6dda91851fe6a11c34cb0af6b0faa3739c92b
Julian W <j.w10@freenet.de>
2026-02-19 14:02:46 +0000

    Deleted dfile

--- bin/xsbench/linux_amd.sh
+++ bin/xsbench/linux_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 1 linux ./bench_xsbench_mt -- -p 2500000 -g 200000
-../launch_benchmark.sh 1 1 linux ./bench_xsbench_mt -- -p 2500000 -g 200000
+../launch_benchmark.sh 0 1 linux ./bench_xsbench_mt -- -p 2500000 -g 100000
+../launch_benchmark.sh 1 1 linux ./bench_xsbench_mt -- -p 2500000 -g 100000
--- bin/xsbench/mitosis_amd.sh
+++ bin/xsbench/mitosis_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 1 mitosis ./bench_xsbench_mt -- -p 2500000 -g 200000
-../launch_benchmark.sh 3 1 mitosis ./bench_xsbench_mt -- -p 2500000 -g 200000
+../launch_benchmark.sh 2 1 mitosis ./bench_xsbench_mt -- -p 2500000 -g 100000
+../launch_benchmark.sh 3 1 mitosis ./bench_xsbench_mt -- -p 2500000 -g 100000
