3433cad2e9688ddace2f11a85956d6fa2b26f398
Julian10 <Julian10@node0.pagetable-amd.pagetable-pg0.clemson.cloudlab.us>
2026-03-07 16:26:47 -0500

    Added results

--- bin/xsbench/linux_amd.sh
+++ bin/xsbench/linux_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 1 linux ./bench_xsbench_mt -- -p 2500000 -g 100000
-../launch_benchmark.sh 1 1 linux ./bench_xsbench_mt -- -p 2500000 -g 100000
+../launch_benchmark.sh 0 1 linux ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 1 1 linux ./bench_xsbench_mt -- -p 25000000 -g 400000
