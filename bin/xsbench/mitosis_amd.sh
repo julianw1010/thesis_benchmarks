2ecdbead823467a116b2fdf563a34937ad476489
Julian10 <Julian10@node0.pagetable-amd.pagetable-pg0.clemson.cloudlab.us>
2026-03-07 16:47:21 -0500

    Updated scripts

--- bin/xsbench/mitosis_amd.sh
+++ bin/xsbench/mitosis_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 1 mitosis ./bench_xsbench_mt -- -p 25000000 -g 400000
-../launch_benchmark.sh 3 1 mitosis ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 2 5 mitosis ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 3 5 mitosis ./bench_xsbench_mt -- -p 25000000 -g 400000
