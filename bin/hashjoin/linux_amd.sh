f3f00f27165c33924b9f3860d2629dc2e9a8fea6
Julian10 <Julian10@node0.pagetable-amd.pagetable-pg0.clemson.cloudlab.us>
2026-03-08 13:53:33 -0400

    Added scripts

--- bin/hashjoin/linux_amd.sh
+++ bin/hashjoin/linux_amd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 amd/linux/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
-../launch_benchmark.sh 1 5 amd/linux/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 0 5 linux ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 1
+../launch_benchmark.sh 1 5 linux ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 1
+
--- bin/hashjoin/mitosis_amd.sh
+++ bin/hashjoin/mitosis_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 2 5 amd/mitosis/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
-../launch_benchmark.sh 3 5 amd/mitosis/200gb ./bench_xsbench_mt -- -p 25000000 -g 400000
+../launch_benchmark.sh 2 5 mitosis ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 7
+../launch_benchmark.sh 3 5 mitosis ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 7
