537742ff9c015a248849e53b0fe839c7d2a5cd13
Julian10 <Julian10@node0.pagetable-amd.pagetable-pg0.clemson.cloudlab.us>
2026-03-08 06:29:21 -0400

    Updated scripts

--- bin/canneal/linux_amd.sh
+++ bin/canneal/linux_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 128 2400000 2000 ../../datasets/canneal_large 2400
-../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 128 2400000 2000 ../../datasets/canneal_large 2400
+../launch_benchmark.sh 0 5 linux ./bench_canneal_mt 128 450000 2000 ../../datasets/canneal_large 260
+../launch_benchmark.sh 1 5 linux ./bench_canneal_mt 128 450000 2000 ../../datasets/canneal_large 260
