9efae631efb35456bd1f3ee90d30e80b3a36bc48
Julian10 <Julian10@node0.pagetable-amd.pagetable-pg0.clemson.cloudlab.us>
2026-03-08 13:55:54 -0400

    Updated script

--- bin/hashjoin/linux_amd.sh
+++ bin/hashjoin/linux_amd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 5 linux ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 1
-../launch_benchmark.sh 1 5 linux ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 1
+../launch_benchmark.sh 0 5 linux ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 7
+../launch_benchmark.sh 1 5 linux ./bench_hashjoin_mt -- -o 1659000000 -i 100000000 -s 100000000 -n 7
 
