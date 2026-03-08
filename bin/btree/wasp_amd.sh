7c5165c02037db85ac4d03445178ac6ee7386842
Julian10 <Julian10@node0.pagetable-amd.pagetable-pg0.clemson.cloudlab.us>
2026-03-08 08:48:18 -0400

    Updated scripts

--- bin/btree/wasp_amd.sh
+++ bin/btree/wasp_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-../launch_benchmark.sh 0 3 wasp ./bench_btree_mt -- -n 1500000000
-../launch_benchmark.sh 1 3 wasp ./bench_btree_mt -- -n 1500000000
+../launch_benchmark.sh 0 5 wasp ./bench_btree_mt -- -n 1500000000
+../launch_benchmark.sh 1 5 wasp ./bench_btree_mt -- -n 1500000000
